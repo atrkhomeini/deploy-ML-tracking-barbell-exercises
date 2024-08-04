755d093e14d90eb7a1d46b7a42113875e3b32344
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2024-08-05 06:03:52 +0700

    chore: Update setup.py description for ML tracking barbell exercises
    This commit updates the description in the setup.py file to provide a more detailed explanation of the project. It mentions that the project is a continuation of the previous ML tracking barbell exercises and that the author is developing it further to be deployed to the public or used for MLOps. The commit also includes other changes like updating the .gitignore and requirements.txt files, adding a new dataclass for managing dataset configuration, and refactoring the common.py module for improved code organization and readability.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__ = "0.0.0"
 
 REPO_NAME = "PSO"
 AUTHOR_USER_NAME = "atrkhomeini"
-SRC_REPO = "cnnClassifier"
+SRC_REPO = "deploy-ML-tracking-barbell-exercises"
 AUTHOR_EMAIL = "5026211156@student.its.ac.id"
 
 
@@ -17,7 +17,7 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
+    description="This is a continuation of the previous ml tracking barbell execises. here the author develops until it can be deployed to the public or can be called MLOps. I wish it'll works",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
